997d20c7a31c509b3ecf9b34b728d546f5c5a051
Giovane <giovane.cardoso@corebiz.ag>
2020-06-26 01:56:31 -0300

    create and read working

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import './App.css';
 const App = ()=> {
   const [ squad, setSquad ] = useState([]) 
   const [ user, setUser ] = useState([])
+  const [ sendUser, setSendUser] = useState({
+    id: "",
+    name: "",
+    lastName: "",
+    age: "",
+    squad: ""
+  })
   
 
   const getSquad = async ()=> {
@@ -18,19 +25,31 @@ const App = ()=> {
   const getUser = async ()=> {
     const { data } = await axios.get("http://localhost:3000/users")
     let user = data
-    // const usuerFiltered = data
     setUser(user)
+  }
+  const submit = (e)=> {
+    e.preventDefault();
+    axios.post("http://localhost:3000/users", sendUser).then(res=> {
+      console.log(res)
+    })
+  }
+  const info = (e)=> {
+    const newUser = {...sendUser}
+    newUser[e.target.id]= e.target.value
+    setSendUser(newUser);
   }
 
   useEffect(()=> {
     getSquad();
-    getUser();
   }, [])
+  useEffect(()=> {
+    getUser();
+  }, [user])
 
   
   return (
     <>
-      <div>
+      <div className="Membros">
         <h1>Membros por squad</h1>
 
         <div>{squad.map(singleSquad => {
@@ -38,11 +57,22 @@ const App = ()=> {
           <>
           <h2>{singleSquad.name}</h2>
           <ul>
-          {user.filter(user => user.squad == singleSquad.id).map(singleUser => <li className="xablau">{singleUser.name}</li>)}
+          {user.filter(user => user.squad == singleSquad.id).map(singleUser => <li className={singleSquad.id}>{singleUser.name}</li>)}
          </ul>
           </>
           )
         })}</div>
+
+      </div>
+      <div className="form" >
+        <form id="form-submit" onSubmit={(e)=> submit(e)}>
+        <input type="number" id="id" name="id" value={sendUser.id} onChange={(e)=>info(e)}></input><br />
+        <input type="text" id="name" name="name" value={sendUser.name} onChange={(e)=>info(e)}></input><br />
+        <input type="text" id="lastName" name="lastName" value={sendUser.lastName} onChange={(e)=>info(e)}></input><br />
+        <input type="text" id="age" name="age" value={sendUser.age} onChange={(e)=>info(e)}></input><br />
+        <input type="text" id="squad" name="squad" value={sendUser.squad} onChange={(e)=>info(e)}></input><br />
+        <input type="submit"></input>
+        </form>
 
       </div>
     </>
