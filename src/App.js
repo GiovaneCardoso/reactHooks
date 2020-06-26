e0a4747482d5ad0374ea3dd44533ebb19f56d136
Giovane <giovane.cardoso@corebiz.ag>
2020-06-26 01:01:46 -0300

    using hooks and learning react structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
+
+//useState = data do vue;
+//useEffect = watch/mounted(depende do 2 param) do vue
+const App = ()=> {
+  const [ squad, setSquad ] = useState([]) 
+  const [ user, setUser ] = useState([])
+  
+
+  const getSquad = async ()=> {
+    const { data } = await axios.get('http://localhost:3000/squads') 
+    let name = data;
+    setSquad(name);
+  }
+  const getUser = async ()=> {
+    const { data } = await axios.get("http://localhost:3000/users")
+    let user = data
+    // const usuerFiltered = data
+    setUser(user)
+  }
+
+  useEffect(()=> {
+    getSquad();
+    getUser();
+  }, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <h1>Membros por squad</h1>
+
+        <div>{squad.map(singleSquad => {
+          return (
+          <>
+          <h2>{singleSquad.name}</h2>
+          <ul>
+          {user.filter(user => user.squad == singleSquad.id).map(singleUser => <li className="xablau">{singleUser.name}</li>)}
+         </ul>
+          </>
+          )
+        })}</div>
+
+      </div>
+    </>
+  )
 }
 
 export default App;
